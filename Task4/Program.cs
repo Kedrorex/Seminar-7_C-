1c6ccd22fe429b5e12c7866683c9e5b55de975fa
Kedrorex <Belousovvk@bk.ru>
2023-03-03 23:10:34 +0400

    inicilizacija

---
+++ Task4/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 51: Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+// Задача 49: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса чётные,
+//  и замените эти элементы на их квадраты.
+
+int[,] FillMatrixWithRandom(int row, int column)
+{
+  int[,] matrix = new int[row, column];
+  Random rnd = new Random();
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rnd.Next(0, 10);
+    }
+  }
+  return matrix;
+}
+
+int GetMatrixQvadart(int[,] matrix)
+{
+  int result = 0;
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (i == j)
+        result += matrix[i, j];
+    }
+  }
+  return result;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      System.Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixWithRandom(row, column);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+int sum = GetMatrixQvadart(matrix);
+Console.WriteLine(sum);
